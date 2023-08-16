4e60013af075f886135f77efdaacab447af03692
Charles Spencer <chucks240@gmail.com>
2023-08-15 19:57:27 -0600

    Test setup for cspencerjr.github.ignored

---
+++ workspace/temp/canvas.js
@@ -0,0 +1,90 @@
+(function () {
+  "use strict";
+  window.addEventListener("load", setupWebGL, false);
+  var gl, program;
+  function setupWebGL(evt) {
+    var source, vertexShader, fragmentShader;
+
+    window.removeEventListener(evt.type, setupWebGL, false);
+    if (!(gl = getRenderingContext())) return;
+
+    var xmlHttp = new XMLHttpRequest();
+
+    vertexShader = gl.createShader(gl.VERTEX_SHADER);
+    xmlHttp.open("GET", "shaders/shader.vert", false);
+    xmlHttp.send();
+    gl.shaderSource(vertexShader, xmlHttp.responseText);
+    gl.compileShader(vertexShader);
+
+    fragmentShader = gl.createShader(gl.FRAGMENT_SHADER);
+    xmlHttp.open("GET", "shaders/shader.frag", false);
+    xmlHttp.send();
+    gl.shaderSource(fragmentShader, xmlHttp.responseText);
+    gl.compileShader(fragmentShader);
+
+    program = gl.createProgram();
+
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+
+    gl.linkProgram(program);
+
+    gl.detachShader(program, vertexShader);
+    gl.detachShader(program, fragmentShader);
+
+    gl.deleteShader(vertexShader);
+    gl.deleteShader(fragmentShader);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+      var linkErrLog = gl.getProgramInfoLog(program);
+      cleanup();
+      document.querySelector("p").innerHTML =
+        "Shader program did not link successfully. " +
+        "Error log: " +
+        linkErrLog;
+      return;
+    }
+    initializeAttributes();
+    gl.useProgram(program);
+    gl.drawArrays(gl.POINTS, 0, 1);
+    cleanup();
+  }
+
+  var buffer;
+  function initializeAttributes() {
+    gl.enableVertexAttribArray(0);
+    buffer = gl.createBuffer();
+    gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
+    gl.bufferData(
+      gl.ARRAY_BUFFER,
+      new Float32Array([0.0, 0.0]),
+      gl.STATIC_DRAW
+    );
+    gl.vertexAttribPointer(0, 2, gl.FLOAT, false, 0, 0);
+  }
+
+  function cleanup() {
+    gl.useProgram(null);
+    if (buffer) gl.deleteBuffer(buffer);
+    if (program) gl.deleteProgram(program);
+  }
+
+  function getRenderingContext() {
+    var canvas = document.querySelector("canvas");
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    var gl =
+      canvas.getContext("webgl") || canvas.getContext("experimental-webgl");
+    if (!gl) {
+      var paragraph = document.querySelector("p");
+      paragraph.innerHTML =
+        "Failed to get WebGL context." +
+        "Your browser or device may not support WebGL.";
+      return null;
+    }
+    gl.viewport(0, 0, gl.drawingBufferWidth, gl.drawingBufferHeight);
+    gl.clearColor(0.0, 0.0, 0.0, 1.0);
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    return gl;
+  }
+})();
